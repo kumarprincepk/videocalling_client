33182065b66d67c7f9b4d2ecfe631ffb4deadfe7
Prince Kumar <princekumar@techugo.com>
2023-10-27 16:13:03 +0530

    render url

--- src/Context.js
+++ src/Context.js
@@ -5,7 +5,8 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 // const socket = io('http://localhost:5000');
-const socket = io('https://videocall-backend-red.vercel.app');
+// const socket = io('https://videocall-backend-red.vercel.app');
+const socket = io('https://videocall-backend-notd.onrender.com');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
